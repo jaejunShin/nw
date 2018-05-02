2bf569dc75fdbb7d36cd8b6e87c3a4ca739eb46f
jaejun <jeajun123123@naver.com>
2018-05-02 16:24:35 +0900

    update basic2.py

--- basic2.py
+++ basic2.py
@@ -1,11 +1,15 @@
 # -*- coding: utf8 -*-
 ## 위 주석을 반드시 추가(한글 읽기)
 
+## 라이브러리 추가
 import urllib2
+import re
 
 ## urllib2 사용 예제
-url = 'http://q02.blogspot.kr/2015/08/info.html'
+url = 'https://box.cdpython.com/ezen/'
 req = urllib2.Request(url)
 res = urllib2.urlopen(req)
 html = res.read()
-print html
+# print html
+## ip주소/포트번호 패턴 찾기 d+.d+.d+.d+/d+ 의 패턴 = 정규표현식
+print re.findall("\d+\.\d+\.\d+\.\d+\/\d+", html)       

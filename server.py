f5837f4dabd669ccb294a095f7dcec773d460521
jaejun <jeajun123123@naver.com>
2018-05-03 17:16:19 +0900

    update server

--- server.py
+++ server.py
@@ -10,9 +10,15 @@ s = socket.socket()
 s.bind(info)                    ## 9999번 포트 바인딩
 s.listen(5)                     ## 바인딩 포트 리스닝
 
-(client, address) = s.accept()  ## 접속 요청 승인
-data = client.recv(1024)        ## 클라이언트 데이터 수신
-print "address %s send data %s" % \
-        (address[0], data)
+while True :
+    (client, address) = s.accept()  ## 접속 요청 승인
+    data = client.recv(1024)        ## 클라이언트 데이터 수신
 
-client.send(data)               ## 클라이언트에 데이터 전송
+    if not data :               ## 예외처리
+        client.close()
+        break
+
+    print "address %s send data %s" % \
+            (address[0], data)
+
+    client.send(data)               ## 클라이언트에 데이터 전송

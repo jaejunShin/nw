a785c4da4839a31c7c6d1b6ad36f44531b5ba387
jaejun <jeajun123123@naver.com>
2018-05-04 11:45:35 +0900

    update server

--- server.py
+++ server.py
@@ -2,8 +2,27 @@
 ## 위 주석을 반드시 추가(한글 읽기)
 
 import socket
+import threading
 
-info = ("127.0.0.1", 9999)
+def handler(client, address) :
+    while True :
+            try:
+                data = client.recv(1024)    ## 클라이언트 데이터 수신
+            except :
+                print "Exception !!!"
+                break
+
+            ## data = client.recv(1024)        ## 클라이언트 데이터 수신
+
+            if not data :                   ## 예외처리
+                client.close()
+                break
+
+            print "address %s send data %s" % (address[0], data)
+
+            client.send(data)               ## 클라이언트에 데이터 전송
+
+info = ("0.0.0.0", 9999)        ## 서버 정보 *.*.*.*
 
 s = socket.socket()
 
@@ -13,20 +32,6 @@ s.listen(5)                     ## 바인딩 포트 리스닝
 while True :
     client, address = s.accept()        ## 접속 요청 승인
     print "[+] new connection from %s(%d)" % (address[0], address[1])
-
-    while True :
-        try:
-            data = client.recv(1024)    ## 클라이언트 데이터 수신
-        except :
-            print "Exception !!!"
-            break
-
-        ## data = client.recv(1024)        ## 클라이언트 데이터 수신
-
-        if not data :                   ## 예외처리
-            client.close()
-            break
-
-        print "address %s send data %s" % (address[0], data)
-
-        client.send(data)               ## 클라이언트에 데이터 전송
+    th = threading.Thread(target = handler, args = (client, address))
+    th.start()
+    

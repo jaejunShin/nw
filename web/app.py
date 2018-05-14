4480c192960f290b423f6c802181bd18f4dab295
jaejun <jeajun123123@naver.com>
2018-05-14 15:39:57 +0900

    update app.py

--- web/app.py
+++ web/app.py
@@ -7,10 +7,38 @@
 
 ## reunder_template : html사용 / request : 요청 처리
 import hashlib
-from flask import Flask, render_template, request
+import sqlite3                                                  ## sql db사용
+from flask import Flask, render_template, request, g            ## g는 전역변수와 유사한 객체
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
-users = {}          ##type - dic
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None :
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext                                ## flask가 종료되는 시점에 사용될 함수.
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False, modify=False):
+    cur = get_db().execute(query, args)
+    if modify:
+        try : 
+            get_db().commit()
+            cur.close()
+        except :
+            return False
+        return True
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv       ## if one이 있으면 return (rv ~ else None), else return rv
+                                                        ## 2개의 if문을 한 줄로 쓰는 방법 지양(return을 하나 쓰기위한 코드)
 
 ## app.route() 호출 후 app.route()가 hello() 호출
 ## ()안의 인자는 주소, "/"는 기본 주소
@@ -23,24 +51,27 @@ def login():
     if request.method == 'POST' :
         id = request.form['id']
         pw = request.form['pw']
-        if id in users :
-            if users[id] == hashlib.sha1(pw).hexdigest() :      ## hash는 항상 고정길이를 지님.
-                return "login ok"
-            else : 
-                return "login fail !!!"
-        else :
-            return "login fail !!!"
+        # if id in users :
+        #     if users[id] == hashlib.sha1(pw).hexdigest() :      ## hash는 항상 고정길이를 지님.
+        #         return "login ok"
+        #     else : 
+        #         return "login fail !!!"
+        # else :
+        #     return "login fail !!!"
 
 @app.route("/join", methods=['GET','POST'])             ## get:주소창에서 접근, post:데이터 전송에 따른 접근
 def join():
     if request.method == 'POST' :
         id = request.form['id']
         pw = request.form['pw']
-        if id not in users :
-            users[id] = hashlib.sha1(pw).hexdigest()
-        else :
-            return "Duplicate!!!"
-        return "join ok"
+        sql = "insert into user(id, password) values ('%s', '%s')" % (id, pw)
+        print sql
+        query_db(sql)
+        # if id not in users :
+        #     users[id] = hashlib.sha1(pw).hexdigest()
+        # else :
+        #     return "Duplicate!!!"
+        # return "join ok"
     return render_template("join.html")
 
 @app.route("/name")
